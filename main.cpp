4db7256609bbd0b8e0f60bb8286dd9d915f5b523
5aquy6uerrsngaf5f8i9477bwa4383sehuam5ad <luat7cckqx8wk1nuwrxg727sdkuk4grr@proton.me>
2023-10-19 19:38:37 +0900

    added identity spoof

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 #include "rustTypes.h"
 #include "utf16-mitigation.h"
 
+#define debugprint(...) //printf(__VA_ARGS__)
+
 u32 findPIDfromProcessName(std::string processNameMultiByteString) {
 	std::wstring processName = widen(processNameMultiByteString);
 
@@ -43,6 +45,63 @@ HANDLE tryToOpen(u32 pid) {
 
 class RemoteProcess {
 public:
+	class ModuleSnapshot {
+	public:
+		class ModuleIterator {
+		private:
+			ModuleIterator(ModuleSnapshot& snapshot, bool ended) : snapshot(snapshot), ended(ended) {
+				if (!ended) {
+					if (!Module32FirstW(snapshot.modulesSnapshot, &snapshot.me32))
+						ended = true;
+				}
+			}
+
+			friend ModuleSnapshot;
+		public:
+			ModuleIterator operator++() {
+				if (!ended) {
+					if (!Module32NextW(snapshot.modulesSnapshot, &snapshot.me32))
+						ended = true;
+				}
+
+				return *this;
+			}
+
+			const MODULEENTRY32W& operator*() const { return snapshot.me32; }
+
+			inline friend bool operator== (const ModuleIterator& a, const ModuleIterator& b) { return a.ended == b.ended; }; // no checks by snapshot
+			inline friend bool operator!= (const ModuleIterator& a, const ModuleIterator& b) { return a.ended != b.ended; };
+		private:
+			ModuleSnapshot& snapshot;
+			bool ended;
+		};
+	public:
+		ModuleSnapshot(u32 pid) {
+			modulesSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, pid);
+			me32.dwSize = sizeof(me32);
+		}
+		~ModuleSnapshot() {
+			if (modulesSnapshot != INVALID_HANDLE_VALUE) {
+				CloseHandle(modulesSnapshot);
+			}
+		}
+
+		ModuleIterator begin() {
+			if (iteratorGiven) throw std::runtime_error("module iterators already has been made");
+			iteratorGiven = true;
+
+			return { *this, false };
+		}
+
+		ModuleIterator end() {
+			return { *this, true };
+		}
+	private:
+		HANDLE modulesSnapshot;
+		MODULEENTRY32W me32;
+		bool iteratorGiven = false;
+	};
+public:
 	RemoteProcess(HANDLE handle) : processHandle(handle) {
 	
 	}
@@ -67,12 +126,24 @@ public:
 		return writeMemory(address, in, sizeof(T));
 	}
 
+	ModuleSnapshot snapshotModules() {
+		return { getPID() };
+	}
+
+	u32 getPID() {
+		if (!pid) {
+			pid = GetProcessId(processHandle);
+		}
+
+		return pid;
+	}
+
 	~RemoteProcess() {
 		CloseHandle(processHandle);
 	}
 protected:
 	HANDLE processHandle;
-
+	u32 pid = 0;
 };
 
 bool stringStartsWith(std::string str, std::string startShouldBe) {
@@ -100,7 +171,7 @@ inline size_t findNextSpace(const std::string& str, size_t offset) {
 	return std::string::npos;
 }
 
-inline std::vector<std::string> splitBySpaces(const std::string& line) {
+std::vector<std::string> splitBySpaces(const std::string& line) {
 	std::vector<std::string> splitted;
 	if (line.size() == 0) return splitted;
 
@@ -114,7 +185,7 @@ inline std::vector<std::string> splitBySpaces(const std::string& line) {
 		while (last < line.size() && isSpace(line[last])) last++; // ignore spaces in a row
 	};
 
-	if (last < line.size() && line[last] != ' ') {
+	if (last < line.size()) {
 		while (last >= 0 && isSpace(line[last])) last--; // trim end
 		splitted.push_back(line.substr(last));
 	}
@@ -156,17 +227,17 @@ cmdLoop:
 			const std::string& command = args[0];
 
 			if (stringStartsWith("exit", command)) break;
-			if (stringStartsWith("reattach", command)) goto start;
+			else if (stringStartsWith("reattach", command)) goto start;
 
 
-			if (stringStartsWith("global", command)) {
+			else if (stringStartsWith("global", command)) {
 				if (argc < 2)
-					throw std::runtime_error("expected address as an argument");
+					throw std::runtime_error("expected lua_state address as an argument");
 
-				size_t address = std::stoull(args[1], nullptr, 16);
+				size_t lstateAddress = std::stoull(args[1], nullptr, 16);
 
 				size_t globalState = 0;
-				studio.readMemory(address + 24, &globalState);
+				studio.readMemory(lstateAddress + 24, &globalState);
 				printf("global state %p\n", globalState);
 
 				size_t ptrEncrKey = globalState + 0xC18 // got from math.random
@@ -174,6 +245,97 @@ cmdLoop:
 
 				u64 newPtrEncrKey[4] = { 1, 0, 0, 0 };
 				studio.writeMemory(ptrEncrKey, &newPtrEncrKey);
+			}
+			else if (stringStartsWith("identityfuck", command)) {
+				/*
+				
+				outdated:
+				lea r8, [rip + ...] is 4c 8d 05 ...
+
+				impersonator = RBX::Security::ThreadImpersonator()
+				if (!RBX::Security::IsInRole(impersonator, Plugin (1))) {
+				  error(piVar8, 1, "create a NetworkClient");
+
+				r8 is 3rd argument (MS __fastcall) so by an error message, RBX::Security::IsInRole can be tracked
+
+				new:
+
+				impersonator = RBX::Security::ThreadImpersonator()
+				newPerms = impersonator[getPermissions](lstate*, impersonator.permissions) -> lstate_impersonator_getPermissions
+				impersonator.permissions = newPerms
+				if (!(newPerms & ...1))
+					RBX::Security::errorThreadPermission(~(byte) newPerms & ...1, char* operation, char* details)
+
+				lstate_impersonator_getPermissions(...):
+					tries to find lua function in stack and to get permissions from it proto->userdata
+					(if no lua function, some global value (hardcoded?) is used)
+					gets permissions from lstate->userdata
+					returns all these permissions & with each other and with original permissions (2nd arg)
+				*/
+
+				RemoteProcess::ModuleSnapshot snapshot = studio.snapshotModules();
+				MODULEENTRY32W mainModule = *snapshot.begin();
+				size_t baseAddr = (size_t) mainModule.modBaseAddr;
+				size_t baseSize = (size_t) mainModule.modBaseSize;
+				
+				debugprint("base image %p %x\n", (void*) baseAddr, baseSize);
+
+
+				// TODO: move to a class? like MemoryReader? (rewrite the shit)
+
+				const u64 bufSize = 4096;
+				u8 buf[bufSize];
+				bool patched = false;
+
+				size_t offset = 0;				
+				while (offset < (baseSize - 1)) {
+					studio.readMemory(baseAddr + offset, &buf, std::min(baseSize - offset, (u64)bufSize));
+
+					for (size_t i = 0; i < bufSize - 7; i++) {
+						if (memcmp(&buf[i], "\x48\x81\xCB\x00\xFF\xFF\xFF", 7) == 0) {
+							u8 codeBuf[256];
+							size_t codePtr = baseAddr + offset + i;
+							debugprint("found [or rbp, -256] (%p)\n", (void*) codePtr);
+							studio.readMemory(codePtr -= (sizeof(codeBuf) / 2), &codeBuf);
+
+							size_t codeBufOffset = (sizeof(codeBuf) / 2);
+							while (codeBufOffset > 0) {
+								if (memcmp(&codeBuf[codeBufOffset], "\xCC\xCC\xCC\xCC", 4) == 0) {
+									while (codeBuf[codeBufOffset] == (u8) '\xCC' && codeBufOffset < sizeof(codeBuf))
+										codeBufOffset++;
+									
+									debugprint("found the start of routine %p\n", (void*) (codePtr + codeBufOffset));
+
+									const u8 payload[] = "\xB8\xFF\xFF\x00\x00\xC3";
+									if (memcmp(&codeBuf[codeBufOffset], payload, sizeof(payload) - 1) != 0) {
+										studio.writeMemory(codePtr + codeBufOffset, payload, sizeof(payload) - 1);
+										printf("patched base+%x\n", (void*) (codePtr + codeBufOffset - baseAddr));
+										patched = true;
+									} else {
+										printf("already patched base+%x\n", (void*) (codePtr + codeBufOffset - baseAddr));
+									}
+
+									break;
+								}
+
+								if (4 > codeBufOffset)
+									codeBufOffset = 0;
+								else
+									codeBufOffset -= 4;
+							}
+						}
+
+						//if (buf[i] == 0x4C && buf[i + 1] == 0x8D && buf[i + 2] == 0x05) {
+						//	i32 ripOffset = *(i32*) (&buf[i + 3]);
+						//	size_t leaReferringTo = baseAddr + offset + i + 7 + ripOffset; // 7 = size of lea
+						//	debugprint("lea r8, [rip + %x] (%p) (%p)\n", ripOffset, leaReferringTo, baseAddr + offset + i);
+					}
+
+					offset += bufSize - 7; // to keep 7 bytes for full lea (or rbp, -256) instruction
+				}
+
+				if (!patched)
+					throw std::runtime_error("couldn't patch any lstate_impersonator::getPermissions");
 			}
 		} catch (const std::exception& e) {
 			std::cerr << "error: " << e.what() << '\n';
